fe5bb696322a50200685bbf0e866dd894a58a94c
Ricardo Rodriguez <ricardo.domingos@gmail.com>
2017-03-10 08:46:53 -0300

    Criação do Feed de sessão

--- web/js/ajax/Session.js
+++ web/js/ajax/Session.js
@@ -8,6 +8,60 @@ $( function() {
 	jsSession.groups			= [ ];
 	jsSession.sessionNameFound 	= false
 	
+	/**
+	 * Load Group by Status
+	 */
+	jsSession.loadSessionsByStatus = function(status,limit,areaHtml){
+		/**
+		 * Execute call to load all groups
+		 */
+		if (window.ajaxLoading)  window.ajaxLoading("show");
+		var loadStatusSessionPath = $("#divLoadSessionByStatus").attr("ajaxurl");
+		var myData     = {'status' : status,'limit' : limit};
+		$.ajax({
+			url:loadStatusGroupsPath,
+			data: myData,
+			type: 'POST',
+			cache: true,
+			
+			error: function(){
+				window.ajaxLoading("hide");
+				
+			},
+			
+			success: function(returned){ 
+				if (window.ajaxLoading)  window.ajaxLoading("hide");
+				areaHtml.empty();
+				var dataout = $.parseJSON(returned);
+
+				if (dataout.result.length == 0) return;
+				myStatus = jsScreenElements.divTitleGroupByStatus(status) ;
+				areaHtml.append(myStatus);
+
+				for(var index=0, len = dataout.result.length; index < len; index++ ){
+				    var myData = dataout.result[index];
+				    var myText = jsScreenElements.divSessionByStatus(status-0);
+				    myText = myText.replace("$sessionName$"   , myData.sessionName );
+				    myText = myText.replace("$sessionDate$"   , myData,sessionDate );
+				    myText = myText.replace("$groupName$", myData.groupName );
+				    areaHtml.append(myText);
+				}
+				
+				return;
+			},
+			
+			statusCode: {
+				404: function() {
+					if (window.ajaxLoading)  window.ajaxLoading("hide");
+					global.msgbox.data('messageBox').danger(window.important, 
+							global.error.connection + loadAllGroupsPath + ". "+ global.error.tryagain);
+				}
+			},
+			
+		});	
+	};
+	
+	
 	jsSession.checkFields = function(){
 		var theMessage = ""; 
 		
