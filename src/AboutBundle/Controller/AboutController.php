15037a2b1f6d6e29f42aea846f967ebc3a403f5f
Ricardo Rodriguez <ricardo.domingos@gmail.com>
2018-01-11 18:29:34 -0200

    Correções finais no AboutController

--- src/AboutBundle/Controller/AboutController.php
+++ src/AboutBundle/Controller/AboutController.php
@@ -14,7 +14,6 @@ class AboutController extends Controller
     {
         return $this->render('AboutBundle:About:index.html.twig');
     }
-<<<<<<< HEAD
     
     /**
      * @Route("/about/projeto")
@@ -22,14 +21,13 @@ class AboutController extends Controller
     public function projetoAction()
     {
         return $this->render('AboutBundle:About:projeto.html.twig');
-=======
 
-    
+    }
     /**
      * @Route("/about/trabalheconosco")
      */
     public function trabalheConoscoAction() {
         return $this->render('AboutBundle:About:trabalheconosco.html.twig');       
->>>>>>> refs/remotes/origin/3.0
+
     }
 }
